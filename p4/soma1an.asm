e48db4521f83ab1b78150bc7242d98dd8077d4fc
Lucas Morato <71903126+lukkas99@users.noreply.github.com>
2023-04-18 17:26:39 -0300

    Update soma1an.asm

--- p4/soma1an.asm
+++ p4/soma1an.asm
@@ -2,17 +2,38 @@
 //n deve ser lido o registrador R0
 //o resultado deve ser armazenado em R1
 
-@0
-D=M
-(LOOP)
 @1 
-D=0
-M=D+1
-@0
-M=D-1
-@fim
-D;JEQ 
-goto LOOP
-(fim)
-@fim
+M = 1 
+
+@SUM
+M = 0
+
+(LOOP)
+@i
+D = M
+@R0
+D = D - M
+@PARE
+D;JGT    //IF ( I > R0) GO TO PARE
+
+@SUM
+D = M
+@i
+D = D + M
+@SUM
+M = D   //SUM = SUM + 1
+
+@i
+M = M + 1    //I = I + 1
+@LOOP
+0;JMP
+
+(PARE)
+@SUM
+D = M
+@R1
+M = D       //R1 = SUM
+
+(FIM)
+@FIM
 0;JMP
